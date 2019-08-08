a05eee04beefa23484dfbc108e485f8f2ec4f8ec
yuki.kawamura <ushikushisakaecho@gmail.com>
2019-08-08 00:41:37 +0000

    add bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(status: 'test status 1', content: 'test content 1')
+Task.create(status: 'test status 2', content: 'test content 2')
+Task.create(status: 'test status 3', content: 'test content 3')
